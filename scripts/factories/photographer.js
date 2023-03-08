16f584122d3b02d0a284344988aec2a6ba119a66
Nellylie <marie.beka01@gmail.com>
2023-03-08 23:08:37 +0100

    deplacement photo + page photographe lié à l'index

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -3,25 +3,80 @@
 
 
 function photographerFactory(data) {
-    // const {name, portrait} = data;
-    // data = Object.fromEntries(new Array (data));
-
-    //simplification pour recuperer les valeurs des cles de chaque photographe
+    
     const name = data.name;
     const portrait = data.portrait;
+    const id = data.id;
+    const tagline = data.tagline;
+    const price = data.price;
+    const city = data.city;
+    const country = data.country;
 
     console.log ("photographerfactory", data);
     const picture = `./assets/photographers/photographers-id/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const aLien = document.createElement('a');
+        aLien.setAttribute("href", `./photographer.html?${id}`);
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        const div = document.createElement("div");
+        div.setAttribute("class", "parent-image");
+        img.setAttribute("class", "imgAvatar");
+        img.setAttribute("src", picture);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
+        const description = document.createElement("p");
+        description.setAttribute("class", "description");
+        description.innerHTML = `${city}, ${country} <br> ${tagline} <br> ${price}€/jour`;
+        div.appendChild(img);
+        aLien.appendChild(div);
+        article.appendChild(aLien);
         article.appendChild(h2);
+        article.appendChild(description);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, id, getUserCardDOM }
 }
+
+
+function coupePrenom(name){
+    let i;
+    let temporaire;
+    for (i = name.length - 1; i>-1; i--){
+        if (name.charAt(i) ==" "){
+            break;
+        }
+    }
+    
+    temporaire = name.toString().substring (0, i);
+    return temporaire;
+}
+
+function photoFactory(data, photographeName) {
+    
+    const urlImage = data.image;
+    const titre = data.title;
+    const name = photographeName.name;
+    const prenom = coupePrenom(name);
+    console.log("prenom", prenom);
+
+    console.log ("photographerfactory", data);
+    const imagePhoto = `./assets/images/${prenom}/${urlImage}`;
+
+    function getPhotoCardDOM() {
+        const figure = document.createElement( "figure" );
+        const figCaption = document.createElement( "figcaption");
+        const img = document.createElement( 'img' );
+        img.setAttribute("class", "imagePhoto");
+        img.setAttribute("src", imagePhoto);
+        const h2 = document.createElement( 'h2' );
+        h2.textContent = titre;
+        figure.appendChild(img);
+        figure.appendChild(figCaption);
+        figCaption.appendChild(h2);
+        return (figure);
+    }
+    return { name, imagePhoto, getPhotoCardDOM }
+}
+
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,58 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function getPhoto() {
+
+    const urlJson = await fetch("./data/photographers.json");
+    const photographersMedia = await urlJson.json();
+    let medias = photographersMedia.media;
+    let photographers = photographersMedia.photographers;
+
+   
+return {medias, photographers};
+}
+
+async function displayData(photo, photographe) {
+    
+    const photoSection = document.querySelector(".photos_section");
+    const photosMedia = photo;
+    const photosPhotographe = photographe;
+    const id = urlRecupere(urlLien);
+    console.log("lid", id);
+    const photosId = photosMedia.filter(photoId => photoId.photographerId == id);
+    console.log("display", photosId);
+    const photosPhotographeName = photosPhotographe.find(photosPhotographeId => photosPhotographeId.id == id);
+
+        photosId.forEach((photoMedia)=>{
+            console.log("boucle photo media", photoMedia); 
+        console.log("liste photographe", photosPhotographe);
+        
+        console.log("photographe", photosPhotographeName);
+        const photoModel = photoFactory(photoMedia, photosPhotographeName);
+        const photoCardDOM = photoModel.getPhotoCardDOM();
+        photoSection.appendChild(photoCardDOM);}
+    );
+};
+
+async function init() {
+    let photoMedia = await (await getPhoto()).medias;
+    let photographers = await (await getPhoto()).photographers;
+    displayData(photoMedia, photographers);
+};
+
+
+init();
+
+const urlLien = window.location.href;
+console.log (urlLien);
+
+function urlRecupere(urlLien){
+    let i;
+    let urlId;
+    for (i = urlLien.length -1; i> -1; i--){
+        if (urlLien.charAt(i - 1) =="?"){
+            break
+        }
+    }
+    urlId = urlLien.substring(i, );
+    return urlId;
+}
+

5c6f5384d0dd63f20dcf1fbee214ebc197d0669e
Nellylie <marie.beka01@gmail.com>
2023-03-16 14:15:35 +0100

    rectification

--- scripts/factories/functions.js
+++ scripts/factories/functions.js
@@ -16,25 +16,11 @@ function coupePrenom(name){
 function reconnaitreMp4(titreImage, titreVideo){
     let remplacement;
     if (titreVideo){
-    remplacement = titreVideo.replace("mp4", "jpg");
+    remplacement = titreVideo.replace(".mp4", ".jpg");
     return remplacement;}
     else {
         return titreImage;
     }
-}
-
-
-function transformeMp4Jpg(titreImage){
-    let i;
-    let temporaire;
-    for (i = titreImage.length - 1; i>-1; i--){
-        if (namtitreImagee.charAt(i) ==" "){
-            break;
-        }
-    }
-    
-    temporaire = titreImage.toString().substring (0, i);
-    return temporaire;
 }
 
 
